3ff4ce803d48ffaed72ddfca503c72be684dd50b
g0dgamerz <jideshvaidya@gmail.com>
2022-07-20 11:34:43 +0545

    added role based authorization

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -46,7 +46,8 @@ namespace jwt.Controllers
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username)
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
             };
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
                 _configuration.GetSection("AppSettings:Token").Value));
